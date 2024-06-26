3a43d9b5178aa4ad6a8f08eb756516724f2e15dc
sahill3 <sahil.mandavkar3@gmail.com>
2024-06-26 19:24:51 +0530

    Formatting changes.

--- solutions/b.cpp
+++ solutions/b.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 void sum(){
     int glo = 9;
-    cout<<"\n"<<glo;
+    cout<<"\n"<<glo << endl;
 }
 
 int main(){
-int a = 45, b = 55, glo = 10;
-//cout<<"Value of a is "<<a<<".\nValue of b is "<<b;
-cout<<glo;
-sum();
+    int a = 45, b = 55, glo = 10;
+
+    cout << "Value of a is "<<a <<endl;
+    cout << "\nValue of b is "<<b <<endl;
+    
+    sum();
+    
+    cout<<glo;
 }
