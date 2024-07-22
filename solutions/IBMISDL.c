4839882db84683a0be418f199ca28b684f55455e
sahill3 <sahil.mandavkar3@gmail.com>
2024-07-22 22:17:53 +0530

    Added the code to find the middle of a linked list

---
+++ solutions/IBMISDL.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    
+    char* temp = "IBMISDL";
+    char * ptr = temp;
+    printf("%d", (int)strlen(temp+2[ptr]-6[ptr]-1));
+
+    return 0;
+}
